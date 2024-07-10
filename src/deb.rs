0e16df2ce78afd927271bb34bb5009e1f1acc59a
Casey Colley <code@caseycolley.com>
2024-07-10 00:23:41 -0700

    setter functions for most except those with PackageLists that need parsing

--- src/deb.rs
+++ src/deb.rs
@@ -4,6 +4,9 @@ use std::hash::Hash;
 use std::str::FromStr;
 use regex::Regex;
 
+mod setters;
+use setters::BINARYDEB_SETTERS;
+
 #[derive(Default)]
 pub struct BinaryDeb
 {
@@ -11,7 +14,7 @@ pub struct BinaryDeb
     source: Option<String>,
     version: String, /* Mandatory */
     section: Option<String>, /* Recommended */
-    priority: Option<String>, /* Recommended */
+    priority: String, /* Recommended */
     architecture: String, /* Mandatory */
     essential: bool,
     depends: Option<DependsPackageList>,
@@ -30,94 +33,107 @@ pub struct BinaryDeb
     built_using: Option<ProvidesPackageList>,
     multi_arch: MultiArch,
     index_fields: Option<BinaryIndexFields>,
-    all_fields: Option<HashMap<String, String>>
+    all_fields: Fields
 }
+
+fn str_to_table (data: &str) -> Result<Fields,PakigeParseError>
+{
+    // https://man7.org/linux/man-pages/man5/deb822.5.html
+    // The field name
+    // is composed of US-ASCII characters excluding control characters,
+    // space, and colon (i.e., characters in the ranges U+0021 ‘!’
+    // through U+0039 ‘9’, and U+003B ‘;’ through U+007E ‘~’,
+    // inclusive).  Field names must not begin with the comment
+    // character (U+0023 ‘#’), nor with the hyphen character (U+002D
+    // ‘-’).
+    let normal_line = Regex::new(r"^[[:space:]]*([[!-9--#---][;-~]][[!-9][;-~]]*)[[:space:]]*:[[:space:]]*(.*)[[:space:]]*$").unwrap();
+    let continuation = Regex::new(r"^( .*)$").unwrap();
+
+    let mut lines: VecDeque<&str> = data.trim().lines().collect();
+    let mut fields: Fields = HashMap::new();
+
+    while let Some(line) = lines.pop_front()
+    {
+        /* There should never be continuation lines in this outer loop */
+        /* The first loop starts with the first line of the stanza, which can not be a continuation line */
+        // Look for normal line, capture key-value pairs
+        if let Some(captures) = normal_line.captures(line)
+        {
+            let key = captures.get(1)
+                .ok_or(PakigeParseError::InvalidFormat)?
+                .as_str()
+                .to_lowercase(); // Note: "Field names are not case-sensitive." RFC 822
+            let value = captures.get(2)
+                .ok_or(PakigeParseError::InvalidFormat)?
+                .as_str();
+
+            // Check for duplicate fields
+            if fields.contains_key(&key)
+            {
+                return Err(PakigeParseError::DuplicateField);
+            }
+
+            // We may need to append further 
+            let mut field_data = String::from(value);
+
+            // Look ahead for continuation lines
+            while let Some(next_line) = lines.pop_front()
+            {
+                if let Some(capture_append) = continuation.captures(next_line)
+                {
+                    let to_append = capture_append.get(1)
+                    .ok_or(PakigeParseError::InvalidFormat)?
+                    .as_str();
+
+                    field_data.push('\n'); // still preserves the double space that indicates to not wrap
+                    field_data.push_str(to_append);
+                }
+                /* Break/Base case */
+                else
+                {
+                    lines.push_front(next_line); // put it back to be dealt with in the outer loop
+                    break;
+                }
+            }
+
+            // Insert into Hashmap
+            fields.insert(key, field_data);
+        }
+        // Else, not valid Debian Control syntax
+        else
+        {
+            return Err(PakigeParseError::InvalidFormat);
+        }
+    }
+
+    if fields.is_empty()
+    {
+        return Err(PakigeParseError::EmptyInput);
+    }
+
+    return Ok(fields);
+}
+
+type Fields = HashMap<String, String>;
 
 impl FromStr for BinaryDeb
 {
     type Err = PakigeParseError;
 
-    // Whitespace can be basically anywhere, but
-    // There is also no requirement to separate keys and values with spaces
-
     fn from_str (data: &str) -> Result<Self, Self::Err>
     {
-        // https://man7.org/linux/man-pages/man5/deb822.5.html
-        // The field name
-        // is composed of US-ASCII characters excluding control characters,
-        // space, and colon (i.e., characters in the ranges U+0021 ‘!’
-        // through U+0039 ‘9’, and U+003B ‘;’ through U+007E ‘~’,
-        // inclusive).  Field names must not begin with the comment
-        // character (U+0023 ‘#’), nor with the hyphen character (U+002D
-        // ‘-’).
-        let normal_line = Regex::new(r"^[[:space:]]*([[!-9--#---][;-~]][[!-9][;-~]]*)[[:space:]]*:[[:space:]]*(.*)[[:space:]]*$").unwrap();
-        let continuation = Regex::new(r"^( .*)$").unwrap();
+        let mut fields = str_to_table(data)?;
+        let mut deb = BinaryDeb {all_fields: fields, ..Default::default()};
 
-        let mut lines: VecDeque<&str> = data.trim().lines().collect();
-        let mut fields: HashMap<String, String> = HashMap::new();
-        // let mut newlines: VecDeque<&str> = VecDeque::new();
-        // // reduce multi-line value into one &str
-        while let Some(line) = lines.pop_front()
-        {
-            /* There should never be continuation lines in this outer loop */
-            /* The first loop starts with the first line of the stanza, which can not be a continuation line */
-            // Look for normal line, capture key-value pairs
-            if let Some(captures) = normal_line.captures(line)
-            {
-                let key = captures.get(1)
-                    .ok_or(PakigeParseError::InvalidFormat)?
-                    .as_str();
-                let value = captures.get(2)
-                    .ok_or(PakigeParseError::InvalidFormat)?
-                    .as_str();
+        // for (key, value) in &fields
+        // {
+        //     println!("\"{}\" : \"{}\"", key, value);
+        // }
 
-                // Check for duplicate fields
-                if fields.contains_key(key)
-                {
-                    return Err(PakigeParseError::DuplicateField);
-                }
+        deb = BINARYDEB_SETTERS.iter()
+        .try_fold(deb, |deb, field| field(deb))?;
 
-                // We may need to append further 
-                let mut field_data = String::from(value);
-
-                // Look ahead for continuation lines
-                while let Some(next_line) = lines.pop_front()
-                {
-                    if let Some(capture_append) = continuation.captures(next_line)
-                    {
-                        let to_append = capture_append.get(1)
-                        .ok_or(PakigeParseError::InvalidFormat)?
-                        .as_str();
-
-                        field_data.push('\n'); // still preserves the double space that indicates to not wrap
-                        field_data.push_str(to_append);
-                    }
-                    /* Break/Base case */
-                    else
-                    {
-                        lines.push_front(next_line); // put it back to be dealt with in the outer loop
-                        break;
-                    }
-                }
-
-                // insert into Hashmap
-                fields.insert(String::from(key.to_lowercase()), field_data);
-            }
-            // Else, not valid Debian Control syntax
-            else
-            {
-                return Err(PakigeParseError::InvalidFormat);
-            }
-        }
-
-        for (key, value) in &fields
-        {
-            println!("\"{}\" : \"{}\"", key, value);
-        }
-
-        // ... TODO: to turn into a struct
-
-        Ok(Default::default())
+        Ok(deb)
     }
 }
 
